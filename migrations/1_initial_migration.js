9e7a7a5026612341cbb2c3aca60b4a01e4d7b77c
Rob <rob.dawson@consensys.net>
2021-07-06 06:17:35 +1000

    working version.

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,6 @@
 var Migrations = artifacts.require('./Migrations.sol')
 
 module.exports = function(deployer) {
+  console.log("Deploying first migration with deployer...", deployer);
   deployer.deploy(Migrations)
 }
--- migrations/3_create_gravatars.js
+++ migrations/3_create_gravatars.js
@@ -1,15 +1,18 @@
 const GravatarRegistry = artifacts.require('./GravatarRegistry.sol')
 
 module.exports = async function(deployer) {
+  console.log(deployer.provider.addresses);
+
+  // throw("NO")
   const registry = await GravatarRegistry.deployed()
 
   console.log('Account address:', registry.address)
 
-  let accounts = await web3.eth.getAccounts()
+  let accounts = deployer.provider.addresses;
   await registry.createGravatar('Carl', 'https://thegraph.com/img/team/team_04.png', {
-    from: accounts[0],
+    from: accounts[1],
   })
   await registry.createGravatar('Lucas', 'https://thegraph.com/img/team/bw_Lucas.jpg', {
-    from: accounts[1],
+    from:  accounts[2],
   })
 }
